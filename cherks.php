6cd4098c5d55e81cc324f4d9172ba6bf78a29c79
Andrés Rozo <carozo908@misena.edu.co>
2020-04-26 12:04:33 -0500

    Gender name "notdef" with value "No definido" was modified for value "Personalizado".

---
+++ cherks.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="css/check.css" type="text/css" />
+<body>
+
+    <div class="wrapper">
+        <form action="" class="forms">
+            <div class="radio">
+                <h2>Radio Buttons</h2>
+                
+                <input type="radio" name="gender" id="man" />
+                <label for="man">Man</label>
+                
+                <input type="radio" name="gender" id="woman" />
+                <label for="woman">Woman</label>
+                
+                <input type="radio" name="gender" id="notdef" />
+                <label for="notdef">Personalizado</label>.
+                
+            </div>
+            
+            <div class="checkbox">
+               <h2>Checkboxing</h2>
+               
+               <input type="checkbox" name="checkbox" id="check1" />
+               <label for="check1">Checkboxing 1</label>
+               
+               <input type="checkbox" name="checkbox" id="check2" />
+               <label for="check2">Checkboxing 2</label>
+                
+            </div>
+            
+        </form>
+    </div>
+    
+</body>
+</html>
