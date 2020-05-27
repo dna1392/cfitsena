ff40dbcc5d521fcdce1993a457c8e35abcb90081
Andrés <carozo908@misena.edu.co>
2020-05-27 10:36:54 -0500

    HOMEPAGE, LOGIN, REGISTRY
    99% (missing data base)

---
+++ js/login/to-string.js
@@ -0,0 +1,10 @@
+function showHide(){
+  var tipo = document.getElementById("password");
+  if(tipo.type == "password"){
+      tipo.type = "text";
+  }else{
+      tipo.type = "password";
+  }
+}
+
+
